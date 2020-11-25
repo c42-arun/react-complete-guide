06945eadfe5d32e488aa10a4833007631d92902e
Arun Ramachandriya <Arun.Ramachandriya@investec.co.uk>
2020-11-25 14:04:53 +0000

    setState()

--- src/App.js
+++ src/App.js
@@ -3,13 +3,39 @@ import './App.css';
 import Person from './Person/Person-functional'; // needs to be upper case Person as lower case elements are reserved for HTML elements
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Arun', age: 20 },
+      {name: 'Jay', age: 30 },
+      {name: 'Jon', age: 40 },
+    ],
+    anotherState: 'Initial state'
+  }
+
+  changeDetailsHandler = () => {
+    // console.log('Clciked!!');
+
+    // this.state.persons[0].name = "Xian"; // DON'T do this, React will not detect
+
+    this.setState({
+      persons: [
+        {name: 'Xian', age: 20 },
+        {name: 'Jay', age: 30 },
+        {name: 'Jon', age: 33 },
+      ],
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>I'm a React app</h1>
-        <Person name='Arun' age='40'/>
-        <Person name='Jay' age='50'/>
-        <Person name='Jon' age='60'>Hobbies: Racing</Person>
+
+        <button onClick={this.changeDetailsHandler}>Update State</button>
+
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Hobbies: Racing</Person>
       </div>
     );
   }

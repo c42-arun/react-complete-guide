c1b39e93983751e3c4a35371c6dea514ee4a3302
Arun Ramachandriya <Arun.Ramachandriya@investec.co.uk>
2020-11-30 10:59:10 +0000

    Updating state immutably

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ class App extends Component {
   }
 
   deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons;
+    /*const persons = this.state.persons.slice(); OR */ 
+    const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
 
     this.setState({ persons: persons});

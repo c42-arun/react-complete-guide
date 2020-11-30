7fdf5fe0722258a9b034f6ef1f5ca1f65223bdf6
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2020-11-30 12:43:03 +0000

    Update list items

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from './Person/Person-functional'; // needs to be upper case Perso
 class App extends Component {
   state = {
     persons: [
-      {name: 'Arun', age: 28 },
-      {name: 'Jay', age: 30 },
-      {name: 'Jon', age: 40 },
+      {id: '123', name: 'Arun', age: 28 },
+      {id: '456', name: 'Jay', age: 30 },
+      {id: '789', name: 'Jon', age: 40 },
     ],
     showPersons: false
   }
@@ -24,6 +24,20 @@ class App extends Component {
     this.setState({ persons: persons});
   }
 
+  nameChangedHandler = (e, id) => {
+    const personIndex = this.state.persons.findIndex(p => p.id === id);
+    const updatePerson = {...this.state.persons[personIndex]}; // copy of person object
+    updatePerson.name = e.target.value;
+
+    const newPersonsList = [...this.state.persons]; // copy of persons array
+    newPersonsList[personIndex] = updatePerson;
+
+    // update state
+    this.setState({
+      persons: newPersonsList
+    });
+  }
+  
   render() {
     const style = {
       backgroundColor: 'white',
@@ -37,15 +51,16 @@ class App extends Component {
 
     if (this.state.showPersons) {
       persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person 
-                    key={index} 
-                    name={person.name} 
-                    age={person.age} 
-                    click={() => this.deletePersonHandler(index)} />
-          })}
-        </div>
+          <div>
+            {this.state.persons.map((person, index) => {
+              return <Person
+                  key={person.id}
+                  name={person.name}
+                  age={person.age}
+                  click={() => this.deletePersonHandler(index)}
+                  changed={(event) => this.nameChangedHandler(event, person.id)}/>
+            })}
+          </div>
       );
     }
 
